3b2f7620645ced1affa1aff824355c84e00b27be
Enr1que319 <59896508+Enr1que319@users.noreply.github.com>
2020-07-06 14:10:36 -0500

    Edit files

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
+import pymongo
+
+# Create a connection to MongoDB
+conn = os.environ.get("MONGODB_URI")
+
+# Client for mongo
+client = pymongo.MongoClient(conn)
+
+# Database
+db = client.videogames
+tectonic_mongo = db.tectonic_plates.find({}, {"_id": 0})
+tectonic_json = []
+
+for result in tectonic_mongo:
+    tectonic_json.append(result)
 
 #################################################
 # Flask Setup
@@ -12,6 +27,12 @@ def home():
 
     key = os.environ.get("API_KEY")
     return render_template("index.html", data=key)
+
+
+@ app.route('/api/v1/tectonic_plates')
+def games():
+
+    return jsonify(tectonic_json)
 
 
 if __name__ == "__main__":
